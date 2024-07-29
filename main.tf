ea9dc44e04efb2940dd759b51f96e5feddac52f6
Abhijit Bole <asbole@hotmail.com>
2024-07-29 12:28:53 +0530

    Updates

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "str1242"
+    storage_account_name  = "str1242"
+    container_name        = "statefile"
+    key                   = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources1"
+  name     = "example-resources2"
   location = "East US"
 }
